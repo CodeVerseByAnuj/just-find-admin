167043f36fdead639989a572de39d3b73cb88142
Anuj Gupta <100862716+CodeVerseByAnuj@users.noreply.github.com>
2025-10-16 00:12:23 +0530

    remove unused file

--- src/lib/schemas/Compliler.schema.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod";
-
-export const CompilerSchema = z.object({
-  source_code: z.string(),
-  language_id: z.number(),
-  stdin: z.string().optional(),
-});
-
-export type CompilerSchemaType = z.infer<typeof CompilerSchema>;
